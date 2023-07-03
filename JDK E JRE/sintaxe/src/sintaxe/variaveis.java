c8ef61287e4d9fd9420aac31e6619f1dc2298257
Bruno <bfsilva98@gmail.com>
2023-07-03 12:43:39 -0300

    added projects

---
+++ JDK E JRE/sintaxe/src/sintaxe/variaveis.java
@@ -0,0 +1,24 @@
+package sintaxe;
+
+public class variaveis {
+
+	public static void main(String[] args) {
+		System.out.println("Ola, novo teste");
+		
+		int idade;
+		idade = 25;
+		
+		double peso;
+		peso = (int)88.5;
+		
+		char letra = 'a';
+		String palavra = "Alura";
+		
+		
+		System.out.println(idade);
+		System.out.println(peso);
+		System.out.println(letra);
+		System.out.println(palavra);
+	}
+
+}

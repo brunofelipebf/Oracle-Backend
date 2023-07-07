a63dda110230002ef56b714755f98c32cc4e3fc9
Bruno <bfsilva98@gmail.com>
2023-07-07 19:16:14 -0300

    First commit

---
+++ bytebank_herdado/src/bytebank_herdado/testeFuncionario.java
@@ -0,0 +1,35 @@
+package bytebank_herdado;
+
+public class testeFuncionario {
+	public static void main(String[] args) {
+		Funcionario bruno = new Funcionario();
+		Gerente Sousa = new Gerente();
+		//caso não seja escrito um construtor, o compilador insere um
+		//por isso a chamada acima funciona
+		
+		//senha para teste
+		int senha = 12;
+		
+		bruno.setNome("Bruno");	
+		bruno.setCpf("12345678919");
+		bruno.setSalario(1400);
+		bruno.setTipo(2);
+		
+		
+		System.out.println(bruno.getNome());
+		System.out.println(bruno.getBonificacao());
+		System.out.println(bruno.getSalario());
+		System.out.println();
+		
+		Sousa.setNome("Sousa");
+		Sousa.setSenha(2556);
+		Sousa.setSalario(2500);
+		boolean autenticou = Sousa.autentica(senha);
+		
+		System.out.println(Sousa.getNome());
+		System.out.println(autenticou);
+		System.out.println(Sousa.getSalario());
+		
+	}
+		
+}
